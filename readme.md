477ae92a902320a5d7a23118ab043c2cf8389db8
Igor Matynia <igor.matynia@gmail.com>
2023-02-11 15:12:25 +0100

    teg api initial commit

---
+++ readme.md
@@ -0,0 +1,29 @@
+# The epic gamer api
+
+This api allows you to conveniently communicate witht the epic gamer api. If you dont know which functions you have 
+access to, you can allways check it at [...]/api/list_funcs
+
+## Requirements
+
+ - python 3
+ - [requests](https://pypi.org/project/requests/)
+
+## Example use
+
+```python
+from teg_api import EpicGamerApi
+
+api_key = "xyz"  # your api key
+url_root = "abc"  # endpoint of the api, defaults to api on localhost
+api = EpicGamerApi(api_key, url_root)
+
+# simple call to api
+resp = api.custom_call("articles", "getallarticles", {}) 
+print(resp.json())
+
+# list all available functions
+resp = api.custom_call("list_funcs")
+
+# simple call on all objects in a json file
+responses = api.call_on_json_file("articles", "addarticle", "all_new_articles_are_in_this_json_file.json")
+```
---
+++ teg_api.py
@@ -0,0 +1,56 @@
+import time
+import requests
+import json
+
+LOCALHOST_ROOT = "http://localhost/api/"
+
+
+class EpicGamerApi:
+    # Question types
+    Q_YES_NO = 0
+    Q_AGREE_SPECTRUM = 1
+
+    def __init__(self, api_key: str, url_root: str = LOCALHOST_ROOT) -> None:
+        self._api_key = api_key
+        self._url_root = url_root
+
+    def list_funcs(self) -> str:
+        data = {}
+        data["api_key"] = self._api_key
+        url = f"{self._url_root}list_funcs"
+        return requests.get(url, params=data).text
+
+    def custom_call(self, model: str, method: str, data: dict = None) -> requests.Response:
+        """
+        :param model: model to run the method on
+        :param method: some method of the model
+        :param data: data to pass to the method ad key-word arguments
+        :return: raw server response
+        """
+        data = data if data else {}
+        data["api_key"] = self._api_key
+        url = f"{self._url_root}{model}.{method}"
+        return requests.get(url, params=data)
+
+    def call_on_json_file(self, model: str, method: str, filename: str, delay: float = 0) -> list[requests.Response]:
+        """
+        Calls EpicGamerApi.custom_call on each element of the list in given json file
+        :param model: model to run the method on
+        :param method: some method of the model
+        :param filename: json file with a list of key-word arument dictionaries
+        :param delay: (optional) delay between each request to api
+        :return: all responses from all requests
+        """
+        with open(filename, "r") as file:
+            datas = json.load(file)
+
+        out = []
+
+        for i, data in enumerate(datas):
+            data["api_key"] = self._api_key
+            ret = self.custom_call(model, method, data)
+            print(f"{i} [{model}.{method}] => {ret.text}")
+            out.append(ret)
+            time.sleep(delay)
+
+        return out
